626c14c73f7243e57089495c50c157954b7f6681
beforecode <yassineuniversite@gmail.com>
2022-09-07 16:58:33 +0200

    polished /Contact US UI

--- pages/contact.js
+++ pages/contact.js
@@ -8,7 +8,7 @@ import Aside from "../components/Aside"
 
 export default function About() {
   return (
-    <div>
+    <div className={c.contact}>
       <Nav />
       <div className={g.static_route}>
         <main>
@@ -17,6 +17,35 @@ export default function About() {
           </div>
           <div className={g.content}>
             <h1>Internationale des Services Publics</h1>
+            <div className={c.top_contact}>
+              <div>
+                <i className="fa-solid fa-location-dot"></i>
+                <div>
+                  <p>45 avenue Voltaire</p>
+                  <p>BP 9</p>
+                  <p>01211 Ferney-Voltaire Cedex</p>
+                  <p>France</p>
+                </div>
+              </div>
+              <div>
+                <i className="fa-solid fa-phone"></i>
+                <div>
+                  <p>Office Tel: +33(0)450406464</p>
+                  <p>Office Fax: +33(0)450407320</p>
+                  <p>Office e-mail: psi@world-psi.org</p>
+                  <p>Web: www.world-psi.org</p>
+                </div>
+              </div>
+            </div>
+            <div className={c.bottom_contact}>
+              <i className="fa-solid fa-envelope"></i>
+              <div>
+                <input type="email" className={c.all_inputs} placeholder="Email" />
+                <input className={c.all_inputs} placeholder="Sujet" />
+                <textarea rows="10" className={c.all_inputs} placeholder="Message"></textarea>
+                <button>Submit</button>
+              </div>
+            </div>
           </div>
         </main>
         <Aside />
