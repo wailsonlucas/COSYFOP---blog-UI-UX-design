78a7a7b30cfc8b463728d097fc06ed820e1b235b
beforecode <yassineuniversite@gmail.com>
2022-09-07 17:29:11 +0200

    finished /Organisations UI

--- pages/friendly-organisations.js
+++ pages/friendly-organisations.js
@@ -16,10 +16,56 @@ export default function About() {
             <p className={g.g_title}>Organisation Amies</p>
           </div>
           <div className={g.content}>
-            dzdz
+            <p>L’Internationale des Services Publics rassemble plus de 20 millions de travailleuses et travailleurs, représentés par 700 syndicats dans 163 pays et territoires.</p>
+            <br />
+            <p>Veuillez utiliser les filtres ci-dessous pour afficher nos organisations affiliées par région, sous-région et/ou pays et/ou territoires</p>
+
+            <div className={f.org}>
+              <div className={f.img_cont}>
+                <img src={"/images/static/org4.png"} alt="" />
+              </div>
+              <div className={f.content}>
+                <h3>Organisation Amie 1</h3>
+                <p>Veuillez utiliser les filtres ci-dessous pour afficher nos organisations affiliées par région, sous-région et/ou pays et/ou territoires</p>
+                <div>visite à <a href="/">www.org-1.com</a></div>
+              </div>
+            </div>
+
+
+            <div className={f.org}>
+              <div className={f.img_cont}>
+                <img src={"/images/static/org1.png"} alt="" />
+              </div>
+              <div className={f.content}>
+                <h3>Organisation Amie 2</h3>
+                <p>Veuillez utiliser les filtres ci-dessous pour afficher nos organisations affiliées par région, sous-région et/ou pays et/ou territoires</p>
+                <div>visite à <a href="/">www.org-2.com</a></div>
+              </div>
+            </div>
+
+            <div className={f.org}>
+              <div className={f.img_cont}>
+                <img src={"/images/static/org2.png"} alt="" />
+              </div>
+              <div className={f.content}>
+                <h3>Organisation Amie 3</h3>
+                <p>Veuillez utiliser les filtres ci-dessous pour afficher nos organisations affiliées par région, sous-région et/ou pays et/ou territoires</p>
+                <div>visite à <a href="/">www.org-3.com</a></div>
+              </div>
+            </div>
+
+            <div className={f.org}>
+              <div className={f.img_cont}>
+                <img src={"/images/static/org3.png"} alt="" />
+              </div>
+              <div className={f.content}>
+                <h3>Organisation Amie 4</h3>
+                <p>Veuillez utiliser les filtres ci-dessous pour afficher nos organisations affiliées par région, sous-région et/ou pays et/ou territoires</p>
+                <div>visite à <a href="/">www.org-4.com</a></div>
+              </div>
+            </div>
+
           </div>
-
-
         </main>
         <Aside />
       </div>
